116774153b65e6726297e7d14144058fb3a9a61b
dbz <dbirt3168@outlook.com>
2017-09-24 21:24:14 -0400

    enhance:styled up the dashboard and updated some of the global styles

--- src/app/services/mock-heros.ts
+++ src/app/services/mock-heros.ts
@@ -5,7 +5,7 @@ export const HEROS : Hero[] = [
     {id: 12, name:'Mr. Craig', description: 'Probably the nicest guy I ever met'},
     {id: 13, name:'Mr. Coop', description: 'Thats a bad mofucka for sure'},
     {id: 14, name:'Mr. Randy', description: 'Fitty Tyson... need I say more?'},
-    {id: 15, name:'Mr. Wilson', description: 'definitely a closet serial killer'},
+    {id: 15, name:'Mr. Wilson', description: 'definitely not a closet serial killer'},
     {id: 16, name:'Mrs. Sara', description: 'The queen of my hert'},
     {id: 17, name:'Mr. Bryant', description: 'B.RAD the baddest programming pupil  I have eva trained'},
     {id: 18, name:'Mr. Dave', description: 'The all around jack of all trades, baddest mothefucka you ever met.'}]
